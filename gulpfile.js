80b1220864e53f68793dc56a786d80f715550a04
Ahmed <dev.ahmed93@gmail.com>
2018-10-11 00:26:36 +0200

    Finish site structure

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const connect = require('gulp-connect');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('watch', function(){
+     gulp.watch('src/**/*.scss', ['sass']);
+     gulp.watch('src/**/*.pug', ['pug']);
+});
+
+gulp.task('sass',  function(){
+    return gulp.src('src/sass/style.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+        }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('assets/css'))
+      .pipe(connect.reload())
+});
+
+gulp.task('pug', function(){
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(connect.reload())
+
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: './',
+        livereload: true,
+        port: 9000
+    });
+});
+
+gulp.task('start', ['watch','connect']);
